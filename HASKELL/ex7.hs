c31e11e00dd1ec5d50c48e4ba689c9e5d95ee66c
Andrea <andybbruno@gmail.com>
2018-11-19 12:09:20 +0100

    Ex 7

--- HASKELL/ex7.hs
+++ HASKELL/ex7.hs
@@ -6,3 +6,30 @@ Goal: Fun with strings and lists (again :P).
 Expected output: Two implementations of countVowelPali: one recursive and one using the combinators map, filter, foldl/r from the Haskell Prelude.
 
 -}
+
+import Data.List
+
+:{
+
+pal s = s == reverse s
+vowels xs = map (\y -> length [ x | x <- y , x `elem` ['a','e','i','o','u']]) xs
+ 
+
+
+countVowelPali x = foldl (+) 0 (vowels (filter pal x))
+
+
+countVowelPali' [] = 0
+countVowelPali' (x:xs) = if pal x 
+    then length [ a | a <- x , a `elem` ['a','e','i','o','u']] + countVowelPali' xs
+    else countVowelPali' xs
+
+:}
+
+
+{-
+
+TRY:
+
+
+-}
